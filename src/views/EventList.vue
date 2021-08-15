09a1449b8e8f0fc1fdad40ae0fc97d7f699ce849
rafael mejia <rafaelm@rafaels-MacBook-Pro.local>
2021-08-14 23:47:03 -0600

    Handling-Errors

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -19,7 +19,12 @@ export default {
     }
   },
   created() {
-    this.$store.dispatch('fetchEvents')
+    this.$store.dispatch('fetchEvents').catch(error => {
+      this.$router.push({
+        name: 'ErrorDisplay',
+        params: { error: error }
+      })
+    })
   },
   computed: {
     // eslint-disable-next-line vue/no-dupe-keys
